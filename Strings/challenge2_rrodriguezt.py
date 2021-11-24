5b2bb65b3ab6665dd2cdeef860c6d4e2f09d19af
Ermys_Code <raul.rodriguez.trotonda@gmail.com>
2021-11-24 12:42:25 +0100

    Lists*

--- Strings/challenge2_rrodriguezt.py
+++
@@ -1,4 +0,0 @@
-print("Wllcome to the Miles Per Hour Conversion App\n")
-
-miles = float(input("What is your speed in miles pero hour: "))
-print(f"Your speed in meters per second is {round(miles * 0.4474, 2)}")
--- Strings/challenge3_rrodriguezt.py
+++
@@ -1,3 +0,0 @@
-fahrenheit = float(input("Introduce la temperatura en ºF: "))
-
-print(f'La temperatura es de {round((fahrenheit - 32) * 5/9, 2)}ºC')
